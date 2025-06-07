31b1a643a80e3717f42ed175768e749aa213d358
Wllynilson <wllynilson@gmail.com>
2025-06-07 14:59:11 -0300

    primeira etapa da configuração do login

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,24 @@
 # app/__init__.py (ATUALIZADO)
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-import click # Importe a biblioteca Click
+import click  # Importe a biblioteca Click
+from flask_login import LoginManager  # Importa a classe
 
 db = SQLAlchemy()
+migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
+    login_manager = LoginManager()
+    login_manager.init_app(app)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../academia.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'sua_chave_secreta_muito_segura_aqui'
 
     db.init_app(app)
-
+    migrate.init_app(app, db)
     from . import models
 
     from .routes import public_bp
@@ -28,6 +34,7 @@ def create_app():
         """Limpa os dados existentes e cria novas tabelas."""
         db.create_all()
         click.echo("Banco de dados inicializado.")
+
     # --- FIM DO NOVO CÓDIGO ---
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
-from app import db  # Importa a instância 'db' do app principal
+from app import db
 from datetime import datetime
+from flask_login import UserMixin # Importa o UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash # Importa as funções de hash
+
 
 
 class Equipe(db.Model):
@@ -48,3 +51,19 @@ class Inscricao(db.Model):
 
     def __repr__(self):
         return f'<Inscricao {self.nome_participante} no evento {self.evento_id}>'
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
