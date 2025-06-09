73fc1058cb0c1915b11e7880913c5565475518c9
Wllynilson <wllynilson@gmail.com>
2025-06-09 13:42:22 -0300

    feat: Adiciona campos de perfil ao User

--- app/models.py
+++ app/models.py
@@ -60,6 +60,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    nome_completo = db.Column(db.String(150), nullable=True)
+    bio = db.Column(db.Text, nullable=True)
+    imagem_perfil = db.Column(db.String(300), nullable=True)  # URL para a imagem de perfil
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
